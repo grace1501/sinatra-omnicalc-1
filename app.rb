b11dd972673f4f256bf24bfbde1541a2c1f0da3e
Grace Do <61570746+grace1501@users.noreply.github.com>
2024-11-19 17:40:15 +0000

    wip - random routes done

--- app.rb
+++ app.rb
@@ -6,20 +6,41 @@ get("/") do
 end
 
 get("/square/new") do
-  @user_number = params.fetch("number").to_i
-  @result = @user_number*@user_number
-
   erb(:square)
 end
+
+get("/square/results") do
+  @user_number = params.fetch("number").to_f
+  @result = @user_number*@user_number
+
+  erb(:square_results)
+end
+
 
 get("/square_root/new") do
   erb(:square_root)
 end
 
-get("/payment/new") do
+get("/square_root/results") do
+  @input_number = params.fetch("user_number").to_f
+  @result = Math.sqrt(@input_number)
 
+  erb(:square_root_results)
 end
 
 get("/random/new") do
+  erb(:random)
+end
 
+get("/random/results") do
+  @min = params.fetch("user_min").to_f
+  @max = params.fetch("user_max").to_f
+  @result = rand(@min..@max)
+  
+  erb(:random_results)
+end
+
+
+get("/payment/new") do
+  erb(:payment)
 end
