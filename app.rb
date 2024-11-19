3200fba2ec91949938bb371051e5a3ca7b6e3072
Grace Do <61570746+grace1501@users.noreply.github.com>
2024-11-19 04:36:16 +0000

    wip - first calculator page

--- app.rb
+++ app.rb
@@ -2,8 +2,24 @@ require "sinatra"
 require "sinatra/reloader"
 
 get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+  erb(:square)
+end
+
+get("/square/new") do
+  @user_number = params.fetch("number").to_i
+  @result = @user_number*@user_number
+
+  erb(:square)
+end
+
+get("/square_root/new") do
+  erb(:square_root)
+end
+
+get("/payment/new") do
+
+end
+
+get("/random/new") do
+
 end
